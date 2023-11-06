1d3adda41cefb735b6bc106b5e858d7d31062771
= <TJBlackman08@gmail.com>
2023-11-06 05:26:10 -0700

    move to index.html

--- main.ts
+++ main.ts
@@ -1,21 +1,7 @@
 Deno.serve(() => {
-  return new Response(
-    `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>SHRUG EMOJI</title>
-    </head>
-    <body>
-      <h1>¯\\_(ツ)_/¯</h1>
-    </body>
-  </html>
-  `,
-    {
-      headers: {
-        "content-type": "text/html; charset=utf-8",
-      },
-    }
-  );
+  return new Response(Deno.readFileSync("./index.html"), {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
+  });
 });
